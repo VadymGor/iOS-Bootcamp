6eecd9d79af5796a044f179286040f9631563534
VadymGor <07tennis@gmail.com>
2020-05-01 19:23:24 +0200

    Start project

---
+++ EggTimer-iOS13/EggTimer/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  EggTimer
+//
+//  Created by Vadym Horovyi on 01/05/2020.
+//  Copyright © 2020 Vadym Horovyi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+
+
+}
