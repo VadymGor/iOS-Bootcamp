3b23515ba0865b00f66d6331a2843545d31154e6
VadymGor <07tennis@gmail.com>
2020-05-04 19:02:10 +0200

    step 1

---
+++ Destini-iOS13/Destini-iOS13/Model/Story.swift
@@ -0,0 +1,9 @@
+//
+//  Sotry.swift
+//  Destini-iOS13
+//
+//  Created by Vadym Horovyi on 04/05/2020.
+//  Copyright © 2020 Vadym Horovyi. All rights reserved.
+//
+
+import Foundation
---
+++ Destini-iOS13/Destini-iOS13/Model/StoryBrain.swift
@@ -0,0 +1,11 @@
+//
+//  StoryBrain.swift
+//  Destini-iOS13
+//
+//  Created by Vadym Horovyi on 04/05/2020.
+//  Copyright © 2020 Vadym Horovyi. All rights reserved.
+//
+
+import Foundation
+
+
