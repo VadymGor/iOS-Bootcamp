e0cba46c960f296f52e3f246cecf75cb19e4fe20
VadymGor <07tennis@gmail.com>
2020-05-08 11:48:00 +0200

    Lesson 17. Actions and Outlets

--- ColorMix/ColorMix/ViewController.swift
+++ ColorMix/ColorMix/ViewController.swift
@@ -13,7 +13,16 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         updateColor()
+        updateControls()
+        
+        //  create border
+        colorView.layer.borderWidth = 5
+        colorView.layer.cornerRadius = 20
+        colorView.layer.borderColor = UIColor.darkGray.cgColor
+        
     }
+    
+    //    MARK: - @IBOutlets
     
     @IBOutlet weak var colorView: UIView!
     
@@ -21,9 +30,36 @@ class ViewController: UIViewController {
     @IBOutlet weak var greenSwitch: UISwitch!
     @IBOutlet weak var blueSwitch: UISwitch!
     
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    
+    
+    //    MARK: - @IBActions
+    
     @IBAction func switchChanged(_ sender: UISwitch) {
         updateColor()
+        updateControls()
     }
+    
+    @IBAction func sliderChanged(_ sender: UISlider) {
+        updateColor()
+    }
+    
+    @IBAction func reset(_ sender: Any) {
+        redSwitch.isOn = false
+        greenSwitch.isOn = false
+        blueSwitch.isOn = false
+        
+        redSlider.value = 0.5
+        greenSlider.value = 0.5
+        blueSlider.value = 0.5
+        
+        updateColor()
+        updateControls()
+    }
+    
+    //    MARK: - Supports
     
     //  metod starts with three var initialized to 0
     func updateColor() {
@@ -31,23 +67,29 @@ class ViewController: UIViewController {
         var green: CGFloat = 0
         var blue:CGFloat = 0
         
-        //  check each switch value and sets the component to 1 if switch is on
+        //  check each switch value of slider and assigns it
         if redSwitch.isOn {
-            red = 1
+            red = CGFloat(redSlider.value)
         }
         if greenSwitch.isOn {
-            green = 1
+            green = CGFloat(greenSlider.value)
         }
         if blueSwitch.isOn {
-            blue = 1
+            blue = CGFloat(blueSlider.value)
         }
         
-//  color is created from the components
-        let color = UIColor(red: red, green: green, blue: blue, alpha: 1)
+        //  color is created from the components
+        let color = UIColor(displayP3Red: red, green: green, blue: blue, alpha: 1)
         
-//  the color of colorView is set
+        //  the color of colorView is set
         colorView.backgroundColor = color
     }
+    
+    //  to make sure all of the sliders are enabled properly
+    func updateControls() {
+        redSlider.isEnabled = redSwitch.isOn
+        greenSlider.isEnabled = greenSwitch.isOn
+        blueSlider.isEnabled = blueSwitch.isOn
+    }
+    
 }
-
-
