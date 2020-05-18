8603fae288a2c1f989a95a8cafdfec254c17d3e8
VadymGor <07tennis@gmail.com>
2020-05-18 18:04:10 +0200

    Complete Section12

---
+++ Tipsy-iOS13/Tipsy/Controllers/ResultViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ResultViewController.swift
+//  Tipsy
+//
+//  Created by Vadym Horovyi on 18/05/2020.
+//  Copyright © 2020 Vadym Horovyi. All rights reserved.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var settingsLabel: UILabel!
+    
+    var result = "0.0"
+    var tip = 10
+    var split = 2
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        totalLabel.text = result
+        settingsLabel.text = "Split between \(split) people, with \(tip) % tip. "
+    }
+
+    @IBAction func recalculatePressed(_ sender: UIButton) {
+        self.dismiss(animated: true, completion: nil)
+    }
+}
