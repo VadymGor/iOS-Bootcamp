eed45caf44cab3f66b9b11f5dfe9cd204a5d64b4
VadymGor <07tennis@gmail.com>
2020-06-18 19:33:01 +0200

    TodoList

---
+++ Todoey-iOS13/Todoey/TodoListViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Vadym Horovyi on 18/06/2020.
+//  Copyright © 2019 App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class TodoListViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+}
+
