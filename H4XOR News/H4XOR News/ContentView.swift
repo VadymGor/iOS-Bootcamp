936f236859cc02889250f8602b8ce5d0b83da0f3
VadymGor <07tennis@gmail.com>
2020-06-18 08:57:52 +0200

    H4X0R News

---
+++ H4XOR News/H4XOR News/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  H4XOR News
+//
+//  Created by Vadym Horovyi on 18/06/2020.
+//  Copyright © 2020 Vadym Horovyi. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        List {
+        Text("Hello, World!")
+        Text("Hello, World!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
